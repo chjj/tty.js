27586f71eee9ca8f8757d03fd05e298256a87fec
Christopher Jeffrey <chjjeffrey@gmail.com>
2012-02-07 05:13:41 -0600

    clean up static/tty.js

--- static/tty.js
+++ static/tty.js
@@ -1,27 +1,26 @@
 ;(function() {
 
-var term;
+var socket
+  , term;
 
 function start() {
   term = new Term(80, 30, handler);
-  // term.convert_lf_to_crlf = true;
   // term.convertEol = true;
 
   term.open();
 }
 
 function handler(ch) {
-  // console.log(JSON.stringify(ch));
   socket.send(ch);
 }
 
 start();
 
-var socket = io.connect('http://127.0.0.1:8080');
+socket = io.connect('http://127.0.0.1:8080');
+
 socket.on('connect', function() {
   socket.on('message', function(data) {
     term.write(data);
-    // console.log(data);
   });
 });
 
